1252de65ce360bd5167f61a5c430d4a153a3f5fc
KentarouTawara <sironagasuken2@gmail.com>
2021-01-02 21:27:34 +0900

    add self-introduction

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
 
   def update_without_current_password(params, *options)
 
-    binding.pry
     if params[:password].blank? && params[:password_confirmation].blank?
       params.delete(:password)
       params.delete(:password_confirmation)
