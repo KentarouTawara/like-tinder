c535f9f4892e7b6017278d05f063e40d45eed7d1
KentarouTawara <sironagasuken2@gmail.com>
2021-01-03 17:59:12 +0900

    cheak action cable

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatRoomChannel", {
+const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -9,11 +9,30 @@ consumer.subscriptions.create("ChatRoomChannel", {
     // Called when the subscription has been terminated by the server
   },
 
+  // chat_room_channel.rbから送られてきたdataを受け取り、アラートを表示
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    return alert(data['chat_message']);
   },
 
-  speak: function() {
-    return this.perform('speak');
+  // chat_room_channel.rbのspeakアクションにchat_messageを送信
+  speak: function(chat_message) {
+    return this.perform('speak', { chat_message: chat_message });
   }
 });
+
+// chat_rooms/が含まれている場合にのみ発動する
+if(/chat_rooms/.test(location.pathname)) {
+
+  // enter を押したら発火する
+  $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
+    if (e.key === "Enter") {
+      // speacアクションを発火
+      appChatRoom.speak(e.target.value);
+      console.log(e)
+      console.log(e.target)
+      console.log(e.target.value)
+      e.target.value = '';
+      e.preventDefault();
+    }
+  })
+}
